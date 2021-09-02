b76b4fa03ec7acd39a37f6a904c14d980be5cc0d
kevinmorales05 <kevin.morales05@epn.edu.ec>
2021-09-01 20:23:15 -0500

    I finish the frond end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button, Input, DatePicker, TimePicker, Space } from "antd";
+import moment from "moment";
+import React, { useState } from "react";
 
 function App() {
+  const [plateNumber, setPlateNumber] = useState("");
+  const [result, setResult] = useState(false);
+  const [time, setTime] = useState("");
+  const [date, setDate] = useState("");
+  const getResults = () => {
+    console.log(plateNumber);
+    if (!result) {
+      setResult(true);
+    } else {
+      setResult(false);
+    }
+    console.log("hora a consultar: ", time);
+  };
+  const onChangeDate = (value, dateString) => {
+    console.log("valor ", value);
+    console.log("fecha escogida ", dateString);
+    setDate(dateString);
+  };
+  const onChangeTime = (value, timeString) => {
+    console.log("valor ", value);
+    console.log("hora escogida ", timeString);
+    setTime(timeString);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="main">
+      <div className="block">
+        <h1 className="title">Adiosmultas.com</h1>
+
+        <p className="subtitle"><i>Evita multas y malos ratos con nuestra app</i></p>
+        <p className="subtitle">
+          Escribe la placa de tu auto, la fecha en la que deseas salir y la
+          hora. Al presionar el botón <b>Analizar</b> la app te dice si puedes salir o no ese día.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Input
+          maxLength="7"
+          className="input"
+          placeholder="Escribe tu placa"
+          onChange={(e) => setPlateNumber(e.target.value)}
+        />
+        <Space direction="vertical">
+          <DatePicker
+            placeholder="Escoge la fecha"
+            dateRender={(current) => {
+              const style = {};
+              if (current.date() === 1) {
+                style.border = "1px solid #1890ff";
+                style.borderRadius = "50%";
+              }
+              return (
+                <div className="ant-picker-cell-inner" style={style}>
+                  {current.date()}
+                </div>
+              );
+            }}
+            onChange={onChangeDate}
+            className="input"
+          />
+          <TimePicker
+            placeholder="Escoge la hora"
+            //defaultValue={moment("12:08", "HH:mm")}
+            size="large"
+            onChange={onChangeTime}
+            className="input"
+          />
+        </Space>
+        <Button className="btn" type="primary" onClick={getResults}>
+          Analizar
+        </Button>
+        {result ? (
+          <div>
+            <p className="subtitle"></p>El resultado es positivo
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/Assets/scripts.js/functions.js
@@ -0,0 +1,3 @@
+export default function lastPlateDigit(plateString) {
+    return plateString[plateString.length - 1]
+}
